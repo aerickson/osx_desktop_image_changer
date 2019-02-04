b98385d586cd1e4c750a87045980051cc118abf2
Andrew Erickson <aerickson@gmail.com>
2019-02-03 17:37:30 -0800

    add favorites

---
+++ favorites-splat.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./change_desktop_on_all_spaces.py $HOME/Downloads/iOS\ Wallpapers/549.Spash4-potassium-1024w-1366h@2x\~ipad.jpg $HOME/Downloads/iOS\ Wallpapers/548.Spash3-potassium-1024w-1366h@2x\~ipad.jpg $HOME/Downloads/iOS\ Wallpapers/546.Spash1-potassium-1024w-1366h@2x\~ipad.jpg $HOME/Downloads/iOS\ Wallpapers/547.Spash2-potassium-1024w-1366h@2x\~ipad.jpg 
