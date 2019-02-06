d4cfd45916e1ff774f8e94ff117f4177a37c0d1e
Andrew Erickson <aerickson@mozilla.com>
2019-02-06 14:41:40 -0800

    update aurora

--- favorites-aurora.sh
+++ favorites-aurora.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./change_desktop_on_all_spaces.py $HOME/Downloads/macOS\ Wallpapers/DefaultDesktop.jpg $HOME/Downloads/macOS\ Wallpapers/Aurora\ \(Server\).jpg $HOME/Downloads/macOS\ Wallpapers/Aurora.jpg 
+./change_desktop_on_all_spaces.py $HOME/Downloads/macOS\ Wallpapers/DefaultDesktop.jpg $HOME/Downloads/macOS\ Wallpapers/Aurora\ \(Server\).jpg $HOME/Downloads/macOS\ Wallpapers/Aurora.jpg  $@
