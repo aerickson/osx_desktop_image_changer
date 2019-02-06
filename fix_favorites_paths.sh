3c20a8ebb1a71d0e062e6cd99aa485bf388ddfba
Andrew Erickson <aerickson@mozilla.com>
2019-02-06 14:16:22 -0800

    cleanup backup file if no diff

--- fix_favorites_paths.sh
+++ fix_favorites_paths.sh
@@ -10,3 +10,13 @@ rpstring="s#$HOME#\$HOME#g"
 
 # for real
 sed -i '.bak' $rpstring $1
+
+# cleanup if .bak is identical
+set +e
+_OUTPUT=`cmp --silent ${1} ${1}.bak`
+RESULT=$?
+set -e
+
+if [ "$RESULT" -eq "0" ]; then
+  rm ${1}.bak
+fi
