ab42eaf6cecf7837a2142d9ed0d4051bbba86601
Andrew Erickson <aerickson@mozilla.com>
2020-03-20 16:13:18 -0700

    fix paths

--- favorites-ios-fewer.sh
+++ favorites-ios-fewer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./change_desktop_on_all_spaces.py /Users/aje/Downloads/iOS\ Wallpapers/1242.WWDC_2019_Dark-1024w-1366h@2x\~ipad.png /Users/aje/Downloads/iOS\ Wallpapers/1241.WWDC_2019_Light-1024w-1366h@2x\~ipad.png /Users/aje/Downloads/iOS\ Wallpapers/1232.WWDC_2019_Dark-1024w-1366h@2x\~ipad.png /Users/aje/Downloads/iOS\ Wallpapers/1211.WWDC_2019_Light-1024w-1366h@2x\~ipad.png /Users/aje/Downloads/iOS\ Wallpapers/1212.WWDC_2019_Dark-1024w-1366h@2x\~ipad.png /Users/aje/Downloads/iOS\ Wallpapers/1231.WWDC_2019_Light-1024w-1366h@2x\~ipad.png $@
+./change_desktop_on_all_spaces.py  $HOME/Downloads/iOS\ Wallpapers/1242.WWDC_2019_Dark-1024w-1366h@2x\~ipad.png  $HOME/Downloads/iOS\ Wallpapers/1241.WWDC_2019_Light-1024w-1366h@2x\~ipad.png  $HOME/Downloads/iOS\ Wallpapers/1232.WWDC_2019_Dark-1024w-1366h@2x\~ipad.png  $HOME/Downloads/iOS\ Wallpapers/1211.WWDC_2019_Light-1024w-1366h@2x\~ipad.png  $HOME/Downloads/iOS\ Wallpapers/1212.WWDC_2019_Dark-1024w-1366h@2x\~ipad.png  $HOME/Downloads/iOS\ Wallpapers/1231.WWDC_2019_Light-1024w-1366h@2x\~ipad.png $@
 
