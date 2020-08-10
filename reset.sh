f61207822cecd4af9780595021cc798d4c751770
Andrew Erickson <aerickson@mozilla.com>
2020-08-10 11:30:47 -0700

    desktop changer: fix reset

--- reset.sh
+++ reset.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 
-killall System\ Preferences > /dev/null 2>&1 && rm ~/Library/Application\ Support/Dock/desktoppicture.db && killall Dock
+# || true because we want to continue running the script if sys prefs wasn't running
+killall System\ Preferences > /dev/null 2>&1 || true
+
+rm ~/Library/Application\ Support/Dock/desktoppicture.db
+
+killall Dock
