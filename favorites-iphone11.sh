c45474608a23c3d903a86d7fb7d596eeff910a1d
Andrew Erickson <aerickson@mozilla.com>
2020-08-10 11:43:23 -0700

    add ios 11 favorites

---
+++ favorites-iphone11.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# look bad... need to rotate?
+
+./change_desktop_on_all_spaces.py $HOME/Downloads/iOS\ Wallpapers/Red-Light.jpg $HOME/Downloads/iOS\ Wallpapers/Yellow-Light.jpg $HOME/Downloads/iOS\ Wallpapers/White-Light.jpg $HOME/Downloads/iOS\ Wallpapers/Purple-Light.jpg $HOME/Downloads/iOS\ Wallpapers/Mint-Light.jpg $HOME/Downloads/iOS\ Wallpapers/Black-Light.jpg $@
+
+
