968bff93bc45d4109f5ace33be3c92286e3d7f4e
Andrew Erickson <aerickson@mozilla.com>
2025-03-04 16:46:02 -0800

    wip 222

--- linkfavs-powder.sh
+++ linkfavs-powder.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
+# set -x
 
-IMAGE_ROOT="$HOME/Downloads/macOS Wallpapers"
+# Files to link (just filenames)
+IMAGES_TO_LINK=(
+    "1.png"
+    "2.png"
+    "3.png"
+)
 
-SYMLINK_TREE_ROOT="$HOME/Downloads/desktop-favorites"
-# base the symlimk tree dir's name on this script's name (remove the .sh)
-SYMLINK_TREE_DEST="${SYMLINK_TREE_ROOT}/$(basename $0 | sed 's/\.sh$//g')"
-
-set -x
-
-mkdir -p ${SYMLINK_TREE_ROOT}
-mkdir -p ${SYMLINK_TREE_DEST}
-
-ln -sf "$IMAGE_ROOT/3.png" "$IMAGE_ROOT/2.png" "$IMAGE_ROOT/1.png" $SYMLINK_TREE_DEST/
+. ./common.sh
+main
